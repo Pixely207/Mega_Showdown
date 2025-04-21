8e0f6c68346342f9a95f1d41082a3156308907e6
Pixely207 <zx147884@gnspes.ca>
2025-04-20 22:06:04 -0300

    Delete neoforge directory

--- neoforge/src/main/java/com/cobblemon/yajatkaul/mega_showdown/item/custom/Memories.java
+++
@@ -1,9 +0,0 @@
-package com.cobblemon.yajatkaul.mega_showdown.item.custom;
-
-import net.minecraft.world.item.Item;
-
-public class Memories extends Item {
-    public Memories(Properties arg) {
-        super(arg);
-    }
-}
